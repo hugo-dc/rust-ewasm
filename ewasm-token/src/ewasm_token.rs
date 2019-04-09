e0e60d7e7b54d4fd7a6f1192f29e016fb991356d
Jose Hugo De la cruz Romero <jhugodc@gmail.com>
2019-04-09 17:50:27 -0500

    fixed types

--- ewasm-token/src/ewasm_token.rs
+++ ewasm-token/src/ewasm_token.rs
@@ -1,16 +1,20 @@
 // see ewasm "WRC20" pseudocode https://gist.github.com/axic/16158c5c88fbc7b1d09dfa8c658bc363
 extern crate ewasm_api;
+use ewasm_api::types::*;
+
+extern "C" {
+    fn storageStore(keyOffset: *const u32, valueOffset: *const u32);
+}
 
 #[no_mangle]
 pub fn main() {
 
-    let data_size = ewasm_api::calldata_size();
-
-    
+    let data_size = ewasm_api::calldata_size(); // calldatasize
     
     if data_size != 24 {
         ewasm_api::revert();
     }
+    
     //                                de   ad   be   ef
     let main_address: Vec<u8> = vec![222, 173, 190, 239,
                                      0,     0,   0,   0,
@@ -18,25 +22,30 @@ pub fn main() {
                                      0,     0,   0,   0,
                                      0,     0,   0,   0];
 
-    let address = ewasm_api::unsafe_calldata_copy(4 as usize, 20 as usize); // calldatacopy
-        /* let expected: [u8;32] = [15, 66, 64, 0, 0, 0, 0, 0,
-                                 0, 0, 0, 0, 0, 0, 0, 0,
-                                 0, 0, 0, 0, 0, 0, 0, 0,
-                                 0, 0, 0, 0, 0, 0, 0, 0];
-         */
-    if main_address == address {
-        let mut sstore_key: [u8;32] = [0;32];
+    let mut address: Vec<u8> = Vec::new();
+
+    ewasm_api::unsafe_calldata_copy(4 as usize, 20 as usize, &mut address); // calldatacopy
+
+    let mut sstore_key = Bytes32 { bytes: [0;32] };
+
         
-        for i in 0..20 {
-            sstore_key[i] = address[i];
-        }
-
-        let balance = ewasm_api::storage_load(&sstore_key); // storageLoad
-        //                       
-
-        //if balance[0] > 0 {
-        ewasm_api::finish_data(&balance);
-        //}
+    for i in 0..address.len() {
+        sstore_key.bytes[i] = address[i];
     }
+
+    let balance = ewasm_api::storage_load(&sstore_key); // storageLoad
+
+    let mut value_test_key = StorageKey {
+        bytes: [0, 0, 0, 0, 0, 0, 0, 0,
+                0,     0,   0,   0, 0, 0, 0, 0,
+                0,     0,   0,   0, 0, 0, 0, 0,
+                0,     0,   0,   0, 0, 0, 0, 1]
+    };
+
+    let storage_value = Bytes32 { bytes: address[0..32] };
+    
+    ewasm_api::storage_store(&value_test_key, &address);
+    
+    ewasm_api::finish_data(&balance.bytes);
     return;
 }
